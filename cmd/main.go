e3e76cb20c981fc7e2c5be07a582b89bdf9866a1
IMNAA <Romaschenko_na@belsbyt.ru>
2026-01-14 22:17:46 +0300

    add func from mono.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"job4j.ru/go-lang-base/internal/base"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	first := 100
+	second := 10
+	res := base.Add(second, first)
+
+	fmt.Println(fmt.Sprintf("%d + %d = %d\n", first, second, res))
+
+	for {
+		fmt.Println(1)
+		break
+	}
+
+	nums := []int{1, 1, 1, 3}
+	fmt.Println(base.Mono(nums))
 }
